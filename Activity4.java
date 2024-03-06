0e6231ccbb41b289b67844ca50b809e1d87d536f
Menard <159229192+menardjakepostanes@users.noreply.github.com>
2024-03-06 14:33:21 +0800

    Add files via upload

--- Activity4.java
+++ Activity4.java
@@ -15,25 +15,25 @@ public class Activity4 {
         
         switch(day){
             case 1:
-                System.out.println("Sunday");
+                System.out.println("JANUARY");
                 break;
             case 2:
-                System.out.println("Monday");
+                System.out.println("FEBUARY");
                  break;
             case 3:
-                System.out.println("Tuesday");
+                System.out.println("MARCH");
                 break;
             case 4:
-                System.out.println("Wednesday");
+                System.out.println("APRIL");
                 break;
             case 5:
-                System.out.println("Thursday");
+                System.out.println("MAY");
                 break;
             case 6:
-                System.out.println("Friday");
+                System.out.println("JUNE");
                 break;
             case 7:
-                System.out.println("Saturday");
+                System.out.println("JULY");
                 break;
     }
 }
